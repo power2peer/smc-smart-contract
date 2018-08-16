a3f3e18bd35187fd9eaa9a59ecfbee50e38fc006
binodpanta <binod80@gmail.com>
2018-08-15 22:11:04 -0400

    update script to be portable

--- sdn_demo/ethereum/ethscripts/gethlocalnet.sh
+++ sdn_demo/ethereum/ethscripts/gethlocalnet.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Use this to start a local geth network which will mine blocks and act as a test server at http://localhost:8545
 WORKDIR=~/ethereum/localnetwork
 mkdir -p $WORKDIR
-./geth-linux-amd64-1.7.3-4bb3c89d/geth --identity "miner1" --networkid 42 --datadir "$WORKDIR" --mine --rpc --rpcport "8545" --port "30303" --unlock 0 --password $WORKDIR/password.sec --ipcpath "~/geth.ipc" --rpcapi "web3,eth,net,debug,personal" --rpccorsdomain "*" --rpcaddr "0.0.0.0"
+echo "12341234" >> $WORKDIR/password.sec
+geth --identity "miner1" --networkid 42 --datadir "$WORKDIR" --mine --rpc --rpcport "8545" --port "30303" --unlock 0 --password $WORKDIR/password.sec --ipcpath "~/geth.ipc" --rpcapi "web3,eth,net,debug,personal" --rpccorsdomain "*" --rpcaddr "0.0.0.0"
