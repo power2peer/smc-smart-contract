ec8e984a892a52e14e9aaf477397723bd4c8161a
binodpanta <binod80@gmail.com>
2018-09-15 22:20:55 -0400

    Formatting fixes

--- transactionapi/src/main/java/com/power2peer/controllers/TransactionController.java
+++ transactionapi/src/main/java/com/power2peer/controllers/TransactionController.java
@@ -2,12 +2,10 @@ package com.power2peer.controllers;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.power2peer.models.EnergyTransaction;
