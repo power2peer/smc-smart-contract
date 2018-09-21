869073349489dae9028679f5b24a938401bea742
binodpanta <binod80@gmail.com>
2018-09-20 22:42:32 -0400

    Add spring data REST to the project
    Also enable /registeredusers endpoint to fetch data

--- transactionapi/src/main/java/com/power2peer/models/RegisteredUserRepository.java
+++ transactionapi/src/main/java/com/power2peer/models/RegisteredUserRepository.java
@@ -2,7 +2,9 @@ package com.power2peer.models;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "registeredusers", path = "registeredusers")
 public interface RegisteredUserRepository extends MongoRepository<RegisteredUser, String> {
 
 	public RegisteredUser findByName(@Param("name") String name);
