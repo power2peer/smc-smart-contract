d67fd5a5fb69578312730988f1925fb66b1ae30f
binodpanta <binod80@gmail.com>
2018-10-03 23:33:48 -0400

    also enable cors on rest data

--- transactionapi/src/main/java/com/power2peer/models/RegisteredUserRepository.java
+++ transactionapi/src/main/java/com/power2peer/models/RegisteredUserRepository.java
@@ -3,7 +3,9 @@ package com.power2peer.models;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "registeredusers", path = "registeredusers")
 public interface RegisteredUserRepository extends MongoRepository<RegisteredUser, String> {
 
