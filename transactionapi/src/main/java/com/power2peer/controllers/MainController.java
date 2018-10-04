9a474051d1aa09a20bf21e39ee2d39a722814f5c
binodpanta <binod80@gmail.com>
2018-10-03 23:24:56 -0400

    add cors to controller

--- transactionapi/src/main/java/com/power2peer/controllers/MainController.java
+++ transactionapi/src/main/java/com/power2peer/controllers/MainController.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.power2peer.models.NewUser;
 import com.power2peer.models.RegisteredUser;
 import com.power2peer.services.UserRegistry;
 
+@CrossOrigin
 @RestController
 public class MainController {
 
