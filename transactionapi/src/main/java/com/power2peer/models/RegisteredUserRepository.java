d792e93882d35b1f2eb422d324751bc602a82472
binodpanta <binod80@gmail.com>
2018-10-29 22:28:46 -0400

    update to provide sortable pageable data for registeredUsers

--- transactionapi/src/main/java/com/power2peer/models/RegisteredUserRepository.java
+++ transactionapi/src/main/java/com/power2peer/models/RegisteredUserRepository.java
@@ -2,6 +2,8 @@ package com.power2peer.models;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource(collectionResourceRel = "registeredusers", path = "registeredusers")
 public interface RegisteredUserRepository extends MongoRepository<RegisteredUser, String> {
 
+	public Page<RegisteredUser> findAll(Pageable pageable);
+	
 	public RegisteredUser findByName(@Param("name") String name);
 
 	public List<RegisteredUser> findByType(@Param("type") UserType type);
